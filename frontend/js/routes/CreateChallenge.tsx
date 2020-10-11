bc9d96e819875fde4fe9f8d6d2a28449367f1d69
Unlocked <10186337+TheUnlocked@users.noreply.github.com>
2020-10-11 13:09:49 -0700

    Basically finish create challenge UI

--- frontend/js/routes/CreateChallenge.tsx
+++ frontend/js/routes/CreateChallenge.tsx
@@ -5,10 +5,11 @@ import { FunctionComponent } from "react";
 import { handleChange } from '../util';
 import SplitPane from 'react-split-pane';
 import Pane from 'react-split-pane/lib/Pane';
-import '../../css/react-split-pane.css';
-import Editor from '@monaco-editor/react';
-import { editor } from 'monaco-editor';
+import { ControlledEditor as Editor, monaco } from '@monaco-editor/react';
+import type { editor } from 'monaco-editor';
 import { Send as SendIcon } from '@material-ui/icons';
+import '../../css/react-split-pane.css';
+import editorTypes from '!!raw-loader!../../assets/challengelib.d.ts';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -36,18 +37,62 @@ const useStyles = makeStyles(theme => ({
         "& > :not(:last-child)": {
             marginRight: theme.spacing(2)
         }
+    },
+    sectionTitle: {
+        lineHeight: 1
+    },
+    sectionCaption: {
+        lineHeight: 1.2,
+        margin: "0.3em 0 0.5em"
     }
 }));
+
+monaco.config({
+    paths: {
+        vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.21.2/min/vs'
+    }
+})
+
+monaco.init().then(async monaco => {
+    monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+        ...monaco.languages.typescript.javascriptDefaults.getCompilerOptions(),
+        lib: ["es2019"]
+    });
+    monaco.languages.typescript.javascriptDefaults.addExtraLib(editorTypes, "challengelib.d.ts");
+});
 
 export const CreateChallenge: FunctionComponent = () => {
     const classes = useStyles();
 
     const [title, setTitle] = useState("New Challenge");
 
+    const [description, setDescription] = useState(
+`# Summary
+In this challenge, you will have to write a Hello, World function.
+
+# Objectives
+- Write a function \`helloWorld\` which returns the string \`"Hello, World!"\``);
+
+    const [starterCode, setStarterCode] = useState(
+`function helloWorld() {
+    // Write your code here:
+\ \ \ \ 
+}`);
+
+    const [solution, setSolution] = useState(
+`function helloWorld() {
+    return "Hello, World!";
+}`);
+
+    const [tests, setTests] = useState(
+`// The tests will fail if any error is thrown
+console.assert(helloWorld() == "Hello, World!");`);
+
     const editorOptions: editor.IEditorConstructionOptions = {
         minimap: { enabled: false },
         renderLineHighlight: "none",
         lineNumbersMinChars: 3,
+        renderWhitespace: 'none',
     };
 
     const editors: editor.IStandaloneCodeEditor[] = [];
@@ -66,22 +111,49 @@ export const CreateChallenge: FunctionComponent = () => {
             <SplitPane className={classes.editors} split="vertical" onChange={() => editors.forEach(x => x.layout())}>
                 <Pane minSize="300px">
                     <Paper className={classes.codeContainer}>
-                        <Typography variant="h6">Explain the Challenge</Typography>
-                        <Editor language="markdown" options={editorOptions} editorDidMount={(_, e) => editors.push(e)}/>
+                        <Typography className={classes.sectionTitle} variant="h6">Explain the Challenge</Typography>
+                        <Typography className={classes.sectionCaption} variant="caption" component="div">Tell the user what their objective is in precise detail</Typography>
+                        <Editor
+                            language="markdown"
+                            options={{...editorOptions, wordWrap: 'on'}}
+                            value={description}
+                            onChange={handleChange(setDescription)}
+                            editorDidMount={(_, e) => editors.push(e)}
+                        />
                     </Paper>
                     <Paper className={classes.codeContainer}>
-                        <Typography variant="h6">Starter Code</Typography>
-                        <Editor language="javascript" options={editorOptions} editorDidMount={(_, e) => editors.push(e)}/>
+                        <Typography className={classes.sectionTitle} variant="h6">Starter Code</Typography>
+                        <Typography className={classes.sectionCaption} variant="caption" component="div">Give the user some code to work with when they begin</Typography>
+                        <Editor
+                            language="javascript"
+                            options={editorOptions}
+                            value={starterCode}
+                            onChange={handleChange(setStarterCode)}
+                            editorDidMount={(_, e) => editors.push(e)}/>
                     </Paper>
                 </Pane>
                 <Pane minSize="300px">
                     <Paper className={classes.codeContainer}>
-                        <Typography variant="h6">Sample Solution</Typography>
-                        <Editor language="javascript" options={editorOptions} editorDidMount={(_, e) => editors.push(e)}/>
+                        <Typography className={classes.sectionTitle} variant="h6">Sample Solution</Typography>
+                        <Typography className={classes.sectionCaption} variant="caption" component="div">Provide a working solution to your challenge. The user will not see this</Typography>
+                        <Editor
+                            language="javascript"
+                            options={editorOptions}
+                            value={solution}
+                            onChange={handleChange(setSolution)}
+                            editorDidMount={(_, e) => editors.push(e)}
+                        />
                     </Paper>
                     <Paper className={classes.codeContainer}>
-                        <Typography variant="h6">Test Code</Typography>
-                        <Editor language="javascript" options={editorOptions} editorDidMount={(_, e) => editors.push(e)}/>
+                        <Typography className={classes.sectionTitle} variant="h6">Test Code</Typography>
+                        <Typography className={classes.sectionCaption} variant="caption" component="div">Provide some code to verify that the user's solution is correct</Typography>
+                        <Editor
+                            language="javascript"
+                            options={editorOptions}
+                            value={tests}
+                            onChange={handleChange(setTests)}
+                            editorDidMount={(_, e) => editors.push(e)}
+                        />
                     </Paper>
                 </Pane>
             </SplitPane>
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             },
+            {
+                test: /\.txt$/,
+                use: "raw-loader"
+            }
         ]
     },
     resolve: {
