66342efb290b330ebd9aaf2e631327bf9b34c7d5
Unlocked <10186337+TheUnlocked@users.noreply.github.com>
2020-10-11 16:23:54 -0700

    login/register API and sandbox function stub

---
+++ backend/sandbox.js
@@ -0,0 +1,8 @@
+/**
+ * Runs untrusted code in a safe execution environment
+ * and returns true if the code throws an error.
+ * @param {string} code 
+ */
+export async function checkCodeThrowsError(code) {
+    return false;
+} 
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,58 @@
 import express from 'express';
 import expressStaticGzip from 'express-static-gzip';
+import passport from 'passport';
 import path from 'path';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { getCollection } from './db.js';
 
 const PORT = 3000;
 
 const app = express();
 
+passport.use(new LocalStrategy(async (username, password, done) => {
+    const users = await getCollection('users');
+    const user = await users.findOne({username, password}, {projection: {
+        _id: 0,
+        username: 1
+    }});
+    if (user) {
+        done(null, user);
+    }
+    else {
+        done(null, false);
+    }
+}));
+
+passport.use('local-signup', new LocalStrategy(async (username, password, done) => {
+    const users = await getCollection('users');
+    const user = await users.findOne({username, password}, {projection: {
+        _id: 0,
+        username: 1
+    }});
+    if (user) {
+        done(null, false);
+    }
+    else {
+        users.insertOne({username, password});
+        done(null, {username});
+    }
+}));
+
+app.post('/register', passport.authenticate('local-signup', {failureRedirect: '/register'}), (req, res) => {
+    res.redirect('/');
+});
+
+app.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), (req, res) => {
+    res.redirect('/');
+});
+
+// Serve files normally if we have them
 app.use(expressStaticGzip("../dist", {
     enableBrotli: true,
     orderPreference: ['br']
 }));
 
+// Send the client otherwise
 app.use('*', (req, res) => {
     res.sendFile(path.resolve("../dist/index.html.br"), {
         headers: {
