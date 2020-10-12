0da1b10651fa1af7042629b1b8bee5a6aaa12408
unknown <jstoessel@wpi.edu>
2020-10-12 02:53:12 -0400

    added a simple server with express, still needs database link

---
+++ server.js
@@ -0,0 +1,34 @@
+// A simple express server for storing settings data
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const { json } = require('body-parser');
+app.use(express.static('./'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+//default get method
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', { root: __dirname })
+})
+
+//skeleton get method
+app.get("/get", (req, res) => {
+    console.log("nothing!");
+    res.json("something from database");
+    res.end();
+})
+
+//skeleton post method
+app.post("/post", bodyParser.json(), (req, res) => {
+    console.log("The nothing is: " + req.body);
+    res.end();
+})
+
+
+//listen to port
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
