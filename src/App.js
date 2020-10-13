b843501d9fae80271d696a9e3b9ac54137ad7e40
Christian Tweed <christiantweed@gmail.com>
2020-10-13 13:32:58 -0400

    Added a demo page and component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Home />
     </div>
   );
 }
