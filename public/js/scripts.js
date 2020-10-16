a0c66078472434c8d9f17403d3ad7bba602091c8
athammana <aj@virpietech.com>
2020-10-16 00:13:10 -0400

    some basic phaser stuff

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,25 +1,95 @@
-console.log('pulled');
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: true
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
-// create Phaser.Game object named "game"
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'my-game',
-    { preload: preload, create: create, update: update });
+let game = new Phaser.Game(config);
 
-// declare global variables for game
+let player, platforms, stars, scoreText, score = 0;
 
-
-// preload game assets - runs once at start
-function preload() {
-
+function preload () {
+	this.load.image('sky', '../assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+	this.load.spritesheet('char', '../assets/dude.png', {frameWidth: 32, frameHeight: 48});
 }
 
-// create game world - runs once after "preload" finished
-function create() {
+function create () {
+	this.add.image(0, 0, 'sky').setOrigin(0, 0);
+	scoreText = this.add.text(16, 16, 'Score: 0', {fontSize: '32px', fill: '#FFF'})
 
+	platforms = this.physics.add.staticGroup();
+    player = this.physics.add.sprite(100, 450, 'char');
+    stars = this.physics.add.group({
+    	key: 'star', repeat: 11, setXY: {x: 12, y: 0, stepX: 70}
+    });
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    stars.children.iterate(child => child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)));
+
+    this.anims.create({
+	    key: 'left',
+	    frames: this.anims.generateFrameNumbers('char', { start: 0, end: 3 }),
+	    frameRate: 10,
+	    repeat: -1
+	});
+
+	this.anims.create({
+	    key: 'turn',
+	    frames: [ { key: 'char', frame: 4 } ],
+	    frameRate: 20
+	});
+
+	this.anims.create({
+	    key: 'right',
+	    frames: this.anims.generateFrameNumbers('char', { start: 5, end: 8 }),
+	    frameRate: 10,
+	    repeat: -1
+	});
+
+	this.physics.add.collider(stars, platforms);
+	this.physics.add.collider(player, platforms);
+	this.physics.add.overlap(player, stars, (player, star) => {
+		star.disableBody(true, true);
+		scoreText.setText('Score: ' + ++score);
+	}, null, this);
 }
 
-// update gameplay - runs in continuous loop after "create" finished
-function update() {
 
+function update () {
+	let cursors = this.input.keyboard.createCursorKeys();
+	if (cursors.left.isDown) {
+	    player.setVelocityX(-160);
+	    player.anims.play('left', true);
+	} else if (cursors.right.isDown) {
+	    player.setVelocityX(160);
+	    player.anims.play('right', true);
+	} else {
+	    player.setVelocityX(0);
+	    player.anims.play('turn');
+	}
+
+	if (cursors.up.isDown && player.body.touching.down){
+	    player.setVelocityY(-330);
+	}
 }
-
-// add custom functions (for collisions, etc.)
