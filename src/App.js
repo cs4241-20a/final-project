472b673c574a420a1b467a89ccdfe4e67c489fcc
TrumanLarson <tlarson@wpi.edu>
2020-10-16 13:07:18 -0400

    Upgraded to secure websocket

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const webrtcProvider = new WebrtcProvider('string-demo', ydoc)
 //Sync clients with the y-websocket provider
 
 const websocketProvider = new WebsocketProvider(
-  `ws://${window.location.host}`, 'string-demo', ydoc
+  `wss://${window.location.host}`, 'string-demo', ydoc
 )
   
 // main component
