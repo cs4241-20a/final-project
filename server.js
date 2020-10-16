4face0dac768bfeee422454653d0020adeaaffe9
TrumanLarson <62713854+TrumanLarson@users.noreply.github.com>
2020-10-16 12:06:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http')
 const wss = new WebSocket.Server({ noServer: true })
 const setupWSConnection = require('./node_modules/y-websocket/bin/utils.js').setupWSConnection
 
-const port = process.env.PORT || 1234
+const port = process.env.YJS_PORT || 1234
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' })
