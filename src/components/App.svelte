7abf2fa4f133093e3a378c6ac1ffd5f883d2e007
Alex Simoneau <afsimoneau@gmail.com>
2020-10-16 21:24:32 -0400

    formatting

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,18 +1,15 @@
-<script>  
-  import { onMount } from 'svelte';
-  
+<script>
+  import { onMount } from "svelte";
+
   import LiteGraph from "./LiteGraph.svelte";
-  
+
   let user = {
     loggedIn: false,
-    username: "not logged in"
+    username: "not logged in",
   };
-  
 
-  
-  
   onMount(async () => {
-    const res = await fetch("/auth/user", {credentials: 'include'});
+    const res = await fetch("/auth/user", { credentials: "include" });
     const results = await res.json();
     if (!results.failed) {
       user.loggedIn = true;
@@ -27,44 +24,46 @@
 <style>
 </style>
 
-<main> 
+<main>
   {#if !user.loggedIn}
-   
-  <nav>
-    
-    <div class="cookie" style= "text-align: center">
-  By using our website, you agree to our 🍪 policy
-             <br>
-    </div>
-  </nav>
-  <div class="prof-center">
-<img width = "200px" src="https://www.wpi.edu/sites/default/files/faculty-image/cdroberts.jpg?1602870867914">
-    
-  </div>
-       
-    <div class="wrapper fadeInDown">
-  <div class="card text-center">
-     
-  <div class="card-body">
-    <h5 class="card-title display-4">Gibber Graph</h5>
-    <p class="card-text">Gibberish <i class="fas fa-heart" style="color: red;"></i> LiteGraph<br></p>
-    <a href="/auth/github" class="btn btn-primary">Login with Github <i class="fab fa-github" ></a>
+    <nav>
+      <div class="cookie" style="text-align: center">
+        By using our website, you agree to our 🍪 policy
+        <br />
       </div>
-
-  <div class="card-footer text-muted">
-
-    <a target="_blank" href="https://github.com/rmanky">Robear Mankaryous</a>, 
-    <a target="_blank" href="https://github.com/afsimoneau">Alexander Simoneau</a>,
-    <a target="_blank" href="https://github.com/kylemikableh">Kyle Mikolajczyk</a>,
-    <a target = "_blank" href = "https://github.com/afreglett">Alexa Freglette 
-  </div>
-
-</div>
-
+    </nav>
+    <div class="prof-center">
+      <img
+        width="200px"
+        src="https://www.wpi.edu/sites/default/files/faculty-image/cdroberts.jpg?1602870867914" />
     </div>
-    
+
+    <div class="wrapper fadeInDown">
+      <div class="card text-center">
+        <div class="card-body">
+          <h5 class="card-title display-4">Gibber Graph</h5>
+          <p class="card-text">
+            Gibberish
+            <i class="fas fa-heart" style="color: red;" />
+            LiteGraph<br />
+          </p>
+          <a href="/auth/github" class="btn btn-primary">Login with Github
+            <i class="fab fa-github" /></a>
+        </div>
+
+        <div class="card-footer text-muted">
+          <a target="_blank" href="https://github.com/rmanky">Robear Mankaryous</a>,
+          <a target="_blank" href="https://github.com/afsimoneau">Alexander
+            Simoneau</a>,
+          <a target="_blank" href="https://github.com/kylemikableh">Kyle
+            Mikolajczyk</a>,
+          <a target="_blank" href="https://github.com/afreglett">Alexa Freglette
+          </a>
+        </div>
+      </div>
+    </div>
   {/if}
   {#if user.loggedIn}
-    <LiteGraph {user}/>
-  {/if} 
+    <LiteGraph {user} />
+  {/if}
 </main>
