b792e84ade4a2d3ff797f20eaa7efb007ca52095
unknown <jstoessel@wpi.edu>
2020-10-16 20:56:31 -0400

    removed friendlist

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,23 +10,21 @@ export class Home extends Component {
         <p>
           <button>Log In</button> | <button>Register</button>
         </p>
-        <p>
-          <button>Friend List</button>
-        </p>
+
         <div class="room">
           <h2>Play BattleShip</h2>
           <button id="hostbtn">Create a Lobby</button>
           <button id="joinrandbtn">Join Random Lobby</button>
-          <button id="joincodebtn">Join Lobby with Code</button>
+          <button id="joincodebtn">Join Private Lobby</button>
           <form>
-            <label>Lobby Join Code:</label>
+            <label>Lobby Join Code: </label>
             <input
               type="text"
               maxlength="4"
               id="joincode"
               name="joincode"
               pattern="\d{4}"
-              placeholder="join code here"
+              placeholder=" Type join code here"
               required
             ></input>
           </form>
