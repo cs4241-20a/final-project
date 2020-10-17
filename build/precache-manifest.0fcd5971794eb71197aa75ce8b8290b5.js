a0d7f2369aa24f67d55033d266ff60c7b7969217
Christian Tweed <christiantweed@gmail.com>
2020-10-16 22:37:39 -0400

    Temporary for glitch

---
+++ build/precache-manifest.0fcd5971794eb71197aa75ce8b8290b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005ad2718913d9eac67b09c95d7dbfed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51ebf87469142bae0569",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "0fadeb35f0bc4786e128",
+    "url": "/static/css/main.65f5122b.chunk.css"
+  },
+  {
+    "revision": "51ebf87469142bae0569",
+    "url": "/static/js/2.b8d8663e.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.b8d8663e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fadeb35f0bc4786e128",
+    "url": "/static/js/main.537918c5.chunk.js"
+  },
+  {
+    "revision": "e59711197170c6140a72",
+    "url": "/static/js/runtime-main.02638610.js"
+  },
+  {
+    "revision": "57f6ac84a92d5a5764af22339d7a93de",
+    "url": "/static/media/cloudlogin.57f6ac84.jpg"
+  },
+  {
+    "revision": "ef270fd113479863754ea2532dc3fc35",
+    "url": "/static/media/cloudregister.ef270fd1.jpg"
+  }
+]);
