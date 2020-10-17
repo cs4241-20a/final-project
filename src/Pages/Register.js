c00b1b9965ff8ab09f8abb18a87bd8e5f54276ee
unknown <jstoessel@wpi.edu>
2020-10-16 20:56:03 -0400

    removed the need for two login css

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,33 +1,32 @@
 import React, { Component } from 'react'
-import './css/register.css'
+import './css/Login.css'
 
 export class Register extends Component {
   render() {
     return (
       <body>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" />
-        <div class="full-screen-container">
-          <div class="login-container">
-            <h3 class="login-title">Register an Account</h3>
+        <div className="full-screen-container2">
+          <div className="login-container">
+            <h3 className="login-title">Register an Account</h3>
             <form action="/register" method="POST">
-              <div class="input-group">
+              <div className="input-group">
                 <label>Username</label>
                 <input type="text" id="username" name="username" placeholder="username here" required/>
               </div>
 
-              <div class="input-group">
+              <div className="input-group">
                 <label>Password</label>
                 <input type="password" id="password" name="password" placeholder="password here" required/>
               </div>
 
-              <div class="input-group">
+              <div className="input-group">
                 <label>Confirm Password</label>
                 <input type="password" id="confirmPass" name="confirmPass" placeholder="confirm password here" required/>
               </div>
 
               <button type="submit" class="login-button">Register</button>
             </form>
-            <div class="container-footer">
+            <div className="container-footer">
               <a href="/login">Click here to Login</a>
             </div>
           </div>
