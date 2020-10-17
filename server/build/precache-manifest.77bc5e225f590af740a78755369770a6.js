492529ceebcecfb2b7d5afda52d48a1f3217019f
Bryce Corbitt <bscorbitt@wpi.edu>
2020-10-17 01:39:33 -0400

    Added webpack build to server

---
+++ server/build/precache-manifest.77bc5e225f590af740a78755369770a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f151a5598c4585241dddeda0c454f74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e01f0765886fbc104b",
+    "url": "/static/css/2.50546774.chunk.css"
+  },
+  {
+    "revision": "d0bf29c7e89536fde2d8",
+    "url": "/static/css/main.76d9df96.chunk.css"
+  },
+  {
+    "revision": "d2e01f0765886fbc104b",
+    "url": "/static/js/2.be841634.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.be841634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0bf29c7e89536fde2d8",
+    "url": "/static/js/main.53e82f9b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
